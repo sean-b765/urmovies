ae66012a47b4d333e677af19a795576e084edf44
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2022-04-21 18:30:21 +0800

    Update

--- components/Comments/Replies.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Reply } from '../../types/common'
-import CommentCard from './Comment'
-
-const Replies: React.FC<{ replies: Reply[] }> = ({ replies }) => {
-	return
-}
-
-export default Replies
